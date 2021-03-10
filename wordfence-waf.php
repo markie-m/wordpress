1bbd2fea27abd50ca601a2c9a9c438bf553bf170
markie_m <marzolfmarkie@gmail.com>
2021-03-10 16:05:46 -0600

    General update

---
+++ wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('/Users/markiemarzolf/Sites/wordpress/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/Users/markiemarzolf/Sites/wordpress/wp-content/wflogs/');
+	include_once '/Users/markiemarzolf/Sites/wordpress/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
+?>
