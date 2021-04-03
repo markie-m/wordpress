a0bb88b19ed6619a79cbcc695c89730973dba770
markie_m <marzolfmarkie@gmail.com>
2021-04-03 14:11:34 -0500

    Added child theme infrastructure

---
+++ wp-content/themes/skillcrushstarter-child/functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Skillcrushstarter Child functions and definitions
+*
+* @link http://codex.wordpress.org/Theme_Development
+* @link http://codex.wordpress.org/Child_Themes
+*
+* @package WordPress
+* @subpackage Skillcrushstarter
+* @since Skillcrushstarter 2.0
+*/
